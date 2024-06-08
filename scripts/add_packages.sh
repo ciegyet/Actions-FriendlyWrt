28f2e68408abce9654fa9b50d34cad3097d649fd
cieyget <1439732340@qq.com>
2024-06-09 05:02:09 +0800

    Update add_packages.sh
    移除不需要的luci

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -22,6 +22,7 @@ EOL
 })
 echo "CONFIG_PACKAGE_luci-theme-argon=y" >> configs/rockchip/01-nanopi
 sed -i -e 's/function init_theme/function old_init_theme/g' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
+
 cat > /tmp/appendtext.txt <<EOL
 function init_theme() {
     if uci get luci.themes.Argon >/dev/null 2>&1; then
@@ -31,4 +32,28 @@ function init_theme() {
 }
 EOL
 sed -i -e '/boardname=/r /tmp/appendtext.txt' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
+
+#修改hostname
+sed -i -e 's/HOSTNAME="FriendlyWrt"/HOSTNAME="Gwrt"/g' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
+
+#移除aria2
+sed -i '/CONFIG_PACKAGE_luci-app-aria2=y/d' configs/rockchip/01-nanopi
+sed -i '/CONFIG_PACKAGE_aria2=y/d' configs/rockchip/01-nanopi
+
+#移除adblock
+sed -i '/CONFIG_PACKAGE_luci-app-adblock=y/d' configs/rockchip/01-nanopi
+
+#移除hd-idle
+sed -i '/CONFIG_PACKAGE_hd-idle=y/d' configs/rockchip/01-nanopi
+sed -i '/CONFIG_PACKAGE_luci-app-hd-idle=y/d' configs/rockchip/01-nanopi
+
+#移除minidlna
+sed -i '/CONFIG_PACKAGE_luci-app-minidlna=y/d' configs/rockchip/01-nanopi
+sed -i '/CONFIG_PACKAGE_minidlna=y/d' configs/rockchip/01-nanopi
+
+#移除watchcat
+sed -i '/CONFIG_PACKAGE_watchcat=y/d' configs/rockchip/01-nanopi
+sed -i '/CONFIG_PACKAGE_luci-app-watchcat=y/d' configs/rockchip/01-nanopi
+
+
 # }}
